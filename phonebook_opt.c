2817ebe931c810e57fadbc573dde419729a74817
BroLeaf <dj3vu6zp@gmail.com>
2018-03-09 02:40:03 +0800

    Reduce entry size and Use hash function

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,16 +1,41 @@
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastName[], entry *pHead)
+
+unsigned long hash(unsigned char *str)
 {
-    /* TODO: implement */
+    unsigned long hash = 5381;
+    int c = 0;
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    return hash;
+}
+
+entry *findName(char lastName[], entry **pArr)
+{
+    entry *pHead = pArr[hash((unsigned char*)lastName) % HASH_SIZE];
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+entry *append(char lastName[], entry **pArr)
 {
-    /* TODO: implement */
-    return NULL;
+    unsigned long index = hash((unsigned char*)lastName) % HASH_SIZE;
+    entry *e = (entry *) malloc(sizeof(entry));
+    e->pNext = pArr[index];
+    pArr[index] = e;
+    strcpy(e->lastName, lastName);
+
+    return e;
 }
+
+
+
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,11 +3,14 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
+
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+
+#define HASH_SIZE 2048
+
+typedef struct __PHONE_BOOK_OTHER {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,10 +20,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} other_entry;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_OTHER *other;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
-entry *findName(char lastName[], entry *pHead);
-entry *append(char lastName[], entry *e);
+entry *findName(char lastName[], entry **pHead);
+entry *append(char lastName[], entry **e);
 
 #endif
